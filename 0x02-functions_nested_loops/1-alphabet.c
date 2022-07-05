d865ef94da617bc10d7de7a26357863a68517f89
Jeremiah <ikechukwujerristar@gmail.com>
2022-07-05 15:48:33 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
- *  Return: Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
