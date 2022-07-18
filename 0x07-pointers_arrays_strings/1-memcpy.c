26f1f9abdd049ec10b786b06b65877cd5bbf2e92
Jeremiah <ikechukwujerristar@gmail.com>
2022-07-17 18:24:24 -0700

    1-memcpy.c file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+/*
+ * File: 1-memcpy.c
+ * Auth: Jeremiah Ikechukwu Obuseri
+ */
+
+#include "main.h"
+
+/**
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
