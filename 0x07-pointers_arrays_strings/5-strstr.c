7d6669404133320a153cbecde5d0e8b442568bab
Jeremiah <ikechukwujerristar@gmail.com>
2022-07-17 18:40:29 -0700

    5-strstr.c file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+/*
+ * File: 5-strstr.c
+ * Auth: Jeremiah Ikechukwu Obuseri
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+
+		if (*two == '\0')
+			return (haystack);
+	}
+
+	return (NULL);
+}
