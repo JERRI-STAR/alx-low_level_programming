c4a62c9d2bb7d19f00ff2c41012e148ca0bb130e
Jeremiah <ikechukwujerristar@gmail.com>
2022-07-19 17:23:45 -0700

    3-factorial.c file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+/*
+ * File: 3-factorial.c
+ * Auth: Jeremiah Ike Obuseri
+ */
+
+#include "main.h"
+
+/**
+ * factorial - youtube c tutorial: 5 - (5 - 1)
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
