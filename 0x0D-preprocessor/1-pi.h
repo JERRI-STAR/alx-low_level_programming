84e49693818916243b7f76c57e6467c7b1bb1b13
Jeremiah <ikechukwujerristar@gmail.com>
2022-08-01 20:40:31 -0700

    /*
     * File: 1-pi.h
    
     * Auth: Brennan D Baraban
    
     * Desc: Header file that defines a macro named PI
    
     *       as an abbreviation for the token 3.14159265359.
    
     */                                       fileclear

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,11 @@
+/*
+* File: 1-pi.h                                                                                                                                         * Auth: Brennan D Baraban                                                                                                   
+* Desc: Header file that defines a macro named PI as an abbreviation for the token 3.1415926535
+*/
+
+#ifndef PI_H
+#define PI_H
+
+#define PI 3.14159265359
+
+#endif /*PI-H*/
