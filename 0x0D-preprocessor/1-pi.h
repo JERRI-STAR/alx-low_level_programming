1c2667d97ebd6c7a63b31032cfe79ab32b820814
Jeremiah <ikechukwujerristar@gmail.com>
2022-08-01 20:43:14 -0700

    1-pi.h file

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,11 +1,13 @@
-/*
-* File: 1-pi.h                                                                                                                                         * Auth: Brennan D Baraban                                                                                                   
-* Desc: Header file that defines a macro named PI as an abbreviation for the token 3.1415926535
-*/
-
 #ifndef PI_H
 #define PI_H
 
+/*
+ * File: 1-pi.h
+ * Auth: Jeremiah Ike Obuseri
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
 #define PI 3.14159265359
 
-#endif /*PI-H*/
+#endif /*PI_H*/
