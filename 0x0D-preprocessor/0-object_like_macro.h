997c818591b8ee5633298dfdcfa532975e37e361
Jeremiah <ikechukwujerristar@gmail.com>
2022-08-01 20:31:43 -0700

    0-object_like_macro.h file

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Jeremiah Ike Obuseri
+ * Desc: A file containing a header file called 0-object_like_macro.h
+ */
+
+#ifndef OBJECT_LIKE_MACRO
+#define OBJECT_LIKE_MACRO
+
+#define SIZE 1024
+
+#endif /* OBJECT_LIKE_MACRO */
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Going into 0x0D-preprocessor
